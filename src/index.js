a14e3bf6a993977db4ce508712ee13a41000ce89
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-11 14:45:50 -0500

    save

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import "./styles.css";
-import { mergeArrays } from "./exercise";
+import { sortWords } from "./exercise";
 
 (() => {
-  const arrayA = [1, 2, 3, 4];
-  const arrayB = [5, 6, 7, 8];
+  const array = ['aa', 'bb', 'z', 'yyyy'];
 
   document.getElementById("app").innerHTML = `
-    <h1>Merge Arrays</h1>
-    <p>Array A: <code>${JSON.stringify(arrayA)}</code></p>
-    <p>Array B: <code>${JSON.stringify(arrayB)}</code></p>
+    <h1>Sort Word</h1>
+    <p>Array: <code>${JSON.stringify(array)}</code></p>
     <p><button id="btn">Run Code</button><p/>
     <p><pre><code id="rta"></code></pre></p>
   `;
@@ -18,7 +16,7 @@ import { mergeArrays } from "./exercise";
 
   runBtn.addEventListener('click', () => {
     try {
-      rtaElement.innerHTML = mergeArrays(arrayA, arrayB);
+      rtaElement.innerHTML = sortWords(array);
     } catch (error) {
       rtaElement.innerHTML = error;
     }
